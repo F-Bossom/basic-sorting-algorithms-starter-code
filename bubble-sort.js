adaa6e8f1446cb2e689e68bf74fded49ece4e2c6
fbossom <felicia.bossom@yahoo.com>
2026-02-24 10:57:32 -0500

    implement bubble sort and insertion sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,20 +1,35 @@
+// Make some sort of loop that keeps happening if there were any swaps
+// that happened this turn
+
+// reset swapHappened to false each time so we can detect if a swap
+// happened in this specific iteration.
+
+// Make another loop (inside the first one) to go through one 
+// round of swapping from the start of the list to the end
+
+// Inside inner loop:
+// compare each pair of elements near each other
+// swap them if the bigger one was at a lower index.
+
+// Make sure to keep track of whether a swap happened!
+
+// After both loops have exited, remember to return the array
 function bubbleSort(array) {
-    // Make some sort of loop that keeps happening if there were any swaps
-    // that happened this turn
+    let swapHappened = true;
 
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
+    while (swapHappened) {
+        swapHappened = false;
 
-    // Make another loop (inside the first one) to go through one 
-    // round of swapping from the start of the list to the end
-    
-    // Inside inner loop:
-    // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapHappened = true;
+            }
+        }
+    }
 
-    // Make sure to keep track of whether a swap happened!
-
-    // After both loops have exited, remember to return the array
     return array;
 }
 
